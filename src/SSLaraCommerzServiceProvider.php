6ec0340dfe712ff96e385d25b4752504765a3681
AfzalSabbir <afzalbd1@gmail.com>
2022-09-11 22:30:13 +0600

    Update SSLaraCommerzServiceProvider.php

--- src/SSLaraCommerzServiceProvider.php
+++ src/SSLaraCommerzServiceProvider.php
@@ -14,7 +14,7 @@ class SSLaraCommerzServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/sslcommerz.php', 'sslaracommerz');
+        $this->mergeConfigFrom(__DIR__ . '/../config/sslcommerz.php', 'sslcommerz');
 
         $this->publishConfig();
         $this->publishViews();
